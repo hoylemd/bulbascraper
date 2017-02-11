930706218faf1399497d884028a5506d61a220d3
Michael D. Hoyle <hoylemd@gmail.com>
2017-02-11 13:20:30 -0500

    scrape links

--- scraper.py
+++ scraper.py
@@ -1,14 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
+egg_groups = {}
+
+BULBAPEDIA_DOMAIN = 'http://bulbapedia.bulbagarden.net'
+
+
+def get_from_bulbapedia(path):
+    return requests.get(BULBAPEDIA_DOMAIN + path).content
+
 
 def scrape_page(url):
     print 'scraping [{}]'.format(url)
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    page = get_from_bulbapedia(url)
+    soup = BeautifulSoup(page, 'html.parser')
     print soup.select('title')
 
-BULBAPEDIA_EGG_GROUPS_URL = 'http://bulbapedia.bulbagarden.net/wiki/Egg_Group'
+    egg_group_links = soup.select('table')[0].select('a')
+
+    for link in egg_group_links:
+        name = link.select('span')[0].string
+        link = link.get('href')
+        egg_groups[name] = link
+
+    print egg_groups
+
+
+BULBAPEDIA_EGG_GROUPS_URL = '/wiki/Egg_Group'
 
 if __name__ == '__main__':
     scrape_page(BULBAPEDIA_EGG_GROUPS_URL)
