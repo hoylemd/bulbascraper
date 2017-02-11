cd3acfc47971c3be1cabd10b6f1c9f011b42ba12
Michael D. Hoyle <hoylemd@gmail.com>
2017-02-11 13:23:19 -0500

    add EggGroup class

--- scraper.py
+++ scraper.py
@@ -6,6 +6,15 @@ egg_groups = {}
 BULBAPEDIA_DOMAIN = 'http://bulbapedia.bulbagarden.net'
 
 
+class EggGroup(object):
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+
+    def __str__(self):
+        return "{}: {}".format(self.name, self.path)
+
+
 def get_from_bulbapedia(path):
     return requests.get(BULBAPEDIA_DOMAIN + path).content
 
@@ -21,9 +30,9 @@ def scrape_page(url):
     for link in egg_group_links:
         name = link.select('span')[0].string
         link = link.get('href')
-        egg_groups[name] = link
-
-    print egg_groups
+        egg_group = EggGroup(name, link)
+        egg_groups[name] = egg_group
+        print egg_group
 
 
 BULBAPEDIA_EGG_GROUPS_URL = '/wiki/Egg_Group'
