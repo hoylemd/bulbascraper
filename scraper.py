54718406842ce02fdb1c662b0d30b58d0f3246ab
Michael D. Hoyle <hoylemd@gmail.com>
2017-02-11 13:53:38 -0500

    add egg group parsing

--- scraper.py
+++ scraper.py
@@ -1,8 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 BULBAPEDIA_DOMAIN = 'http://bulbapedia.bulbagarden.net'
+
+
+def get_from_bulbapedia(path):
+    print 'scraping [{}]'.format(path)
+    page = requests.get(BULBAPEDIA_DOMAIN + path).content
+    return BeautifulSoup(page, 'html.parser')
 
 
 class Pokemon(object):
@@ -15,8 +20,8 @@ class Pokemon(object):
         self.hatch_time_min = 0
         self.hatch_time_max = 0
 
-    def __str__(self):
-        return '{}: {}'.format(self.name, self.path)
+    def __unicode__(self):
+        return u"{}: {}".format(self.name, self.path)
 
 
 class EggGroup(object):
@@ -24,20 +29,29 @@ class EggGroup(object):
         self.name = name
         self.path = path
 
-    def __str__(self):
-        return "{}: {}".format(self.name, self.path)
+        self.pokemon = {}
 
+    def parse(self):
+        soup = get_from_bulbapedia(self.path)
+        pokemon_tables = soup.select('table.roundy table')
 
-def get_from_bulbapedia(path):
-    print 'scraping [{}]'.format(path)
-    return requests.get(BULBAPEDIA_DOMAIN + path).content
+        for pokemon_row in pokemon_tables[0].select('tr')[1:]:
+            link = pokemon_row.select('td:nth-of-type(3) a')[0]
+            name = link.string
+            path = link.get('href')
+            pokemon = Pokemon(name, path)
+            pokemon.egg_groups.append(self)
+            self.pokemon[name] = pokemon
+            print u"{}".format(pokemon)
+
+    def __unicode__(self):
+        return u"{}: {}".format(self.name, self.path)
 
 
 def get_egg_groups(url):
     egg_groups = {}
 
-    page = get_from_bulbapedia(url)
-    soup = BeautifulSoup(page, 'html.parser')
+    soup = get_from_bulbapedia(url)
 
     egg_group_links = soup.select('table')[0].select('a')
 
@@ -54,5 +68,6 @@ BULBAPEDIA_EGG_GROUPS_URL = '/wiki/Egg_Group'
 
 if __name__ == '__main__':
     egg_groups = get_egg_groups(BULBAPEDIA_EGG_GROUPS_URL)
-    for egg_group in egg_groups:
-        print egg_groups[egg_group]
+    for name in egg_groups:
+        group = egg_groups[name]
+        group.parse()
