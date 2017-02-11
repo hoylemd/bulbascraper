3fed7aa1944c6557c9784ff8bb6bb6efa632b766
Michael D. Hoyle <hoylemd@gmail.com>
2017-02-11 12:49:11 -0500

    start scraper

---
+++ scraper.py
@@ -0,0 +1,9 @@
+
+def scrape_page(url):
+    print '{} scraped!'.format(url)
+
+
+BULBAPEDIA_EGG_GROUPS_URL = 'http://bulbapedia.bulbagarden.net/wiki/Egg_Group'
+
+if __name__ == '__main__':
+    scrape_page(BULBAPEDIA_EGG_GROUPS_URL)
