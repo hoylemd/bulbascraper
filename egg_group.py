2bea0c7e5495a651b558b3849840d3001c4b0502
Michael D. Hoyle <hoylemd@gmail.com>
2017-02-14 19:11:48 -0500

    improve egg group parsing

--- egg_group.py
+++ egg_group.py
@@ -11,11 +11,16 @@ class EggGroup(Parsable):
     def parse(self):
         soup = super(EggGroup, self).parse(complete=False)
 
-        pokemon_tables = soup.select('table.roundy table')
+        content = soup.find(id='mw-content-text')
+        pokemon_tables = content.find_all('table', recursive=False)[1:3]
 
-        for pokemon_row in pokemon_tables[0].select('tr')[1:]:
-            # TODO fix this
-            link = pokemon_row.select('td:nth-of-type(3) a')[0]
+        just_this_group = pokemon_tables[0]
+        this_and_other_group = pokemon_tables[1]
+
+        rows = just_this_group.table.find_all('tr')[1:]  # skip heading row
+        rows += this_and_other_group.table.find_all('tr')[1:]
+        for pokemon_row in rows:
+            link = pokemon_row.find_all('td', recursive=False)[2].a
             pokemon = Pokemon(link, egg_group=self)
             self.pokemon[pokemon.slug] = pokemon
 
